f5890f64d62a381f05529a4ed2e7a754a6cd9392
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-12-27 14:05:45 +0100

    adds exitCode and timeout handling

--- src/test/java/de/lgohlke/ci/ShellExecutorTest.java
+++ src/test/java/de/lgohlke/ci/ShellExecutorTest.java
@@ -1,11 +1,17 @@
 package de.lgohlke.ci;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
 
+import static java.time.temporal.ChronoUnit.MILLIS;
 import static org.assertj.core.api.Assertions.assertThat;
 
 class ShellExecutorTest {
@@ -18,12 +24,39 @@ class ShellExecutorTest {
         assertThat(output).contains("HOME=/home");
     }
 
-    private static class ShellExecutor {
-        private static String execute(String command) throws IOException {
-            String[] wrappedInShell = new String[]{"sh", "-c", command};
+    @Test
+    void captureExitCode() {
+        String command = "exit 2";
 
-            Process process = new ProcessBuilder(wrappedInShell)
-                    .start();
+        Result result = ShellExecutor.execute2(command);
+
+        assertThat(result.getExitCode()).isEqualTo(2);
+    }
+
+    @Test
+    void commandKilledWithTimeout() {
+        String command = "sleep 1200";
+
+        Result result = ShellExecutor.execute3(command, Duration.of(100, MILLIS));
+
+        assertThat(result.getExitCode()).isEqualTo(137);
+    }
+
+    @Test
+    void commandExitBeforeTimeout() {
+        String command = "exit 3";
+
+        Result result = ShellExecutor.execute3(command, Duration.of(100, MILLIS));
+
+        assertThat(result.getExitCode()).isEqualTo(3);
+
+    }
+
+    private static class ShellExecutor {
+
+        private static String execute(String command) throws IOException {
+            ProcessBuilder processBuilder = createWrappedInShell(command);
+            Process process = processBuilder.start();
 
             BufferedReader reader =
                     new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -35,5 +68,55 @@ class ShellExecutorTest {
             }
             return builder.toString();
         }
+
+        private static ProcessBuilder createWrappedInShell(String command) {
+            String[] wrappedInShell = new String[]{"sh", "-c", command};
+
+            return new ProcessBuilder(wrappedInShell);
+        }
+
+        @SneakyThrows
+        private static Result execute2(String command) {
+            Process process = createWrappedInShell(command).start();
+
+            BufferedReader reader =
+                    new BufferedReader(new InputStreamReader(process.getInputStream()));
+            StringBuilder builder = new StringBuilder();
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                builder.append(line);
+                builder.append(System.getProperty("line.separator"));
+            }
+            String stdout = builder.toString();
+            return new Result(process.waitFor(), stdout);
+        }
+
+        @SneakyThrows
+        private static Result execute3(String command, Duration maxDuration) {
+
+            Process process = createWrappedInShell(command).start();
+            String stdout = "";
+            long seconds = maxDuration.getSeconds();
+            int nano = maxDuration.getNano();
+            long nanos_to_millis = TimeUnit.NANOSECONDS.toMillis(nano);
+            long seconds_to_millis = TimeUnit.SECONDS.toMillis(seconds);
+
+            long timeout = nanos_to_millis + seconds_to_millis;
+            boolean exitedBeforeTimeout = process.waitFor(timeout, TimeUnit.MILLISECONDS);
+
+            if (!exitedBeforeTimeout) {
+                int exitCode = process.destroyForcibly()
+                                      .waitFor();
+                return new Result(exitCode, stdout);
+            }
+            return new Result(process.waitFor(), stdout);
+        }
+    }
+
+    @RequiredArgsConstructor
+    @Getter
+    private static class Result {
+        private final int exitCode;
+        private final String stdout;
     }
 }

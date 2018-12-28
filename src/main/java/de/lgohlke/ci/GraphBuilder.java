606498b51a0d902b4fee944be7efdde02b0848c8
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-12-28 15:48:01 +0100

    moved some classes from test to main

---
+++ src/main/java/de/lgohlke/ci/GraphBuilder.java
@@ -0,0 +1,55 @@
+package de.lgohlke.ci;
+
+import de.lgohlke.ci.config.BuildConfigReader;
+import de.lgohlke.ci.config.dto.BuildConfig;
+import de.lgohlke.ci.config.dto.Step;
+import de.lgohlke.ci.graph.ExecutionGraph;
+import de.lgohlke.ci.graph.Job;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class GraphBuilder {
+    static ExecutionGraph build(String yaml) {
+
+        BuildConfig buildConfig = BuildConfigReader.parse(yaml);
+
+        Map<String, Step> stepMap = new HashMap<>();
+
+        buildConfig.getSteps()
+                   .forEach(step -> stepMap.put(step.getName(), step));
+
+        Map<String, Job> jobMap = new HashMap<>();
+
+        fillJobMapWithoutDependencies(stepMap, jobMap);
+        resolveJobDependencies(stepMap, jobMap);
+
+        ExecutionGraph.Builder builder = new ExecutionGraph.Builder();
+        jobMap.values()
+              .forEach(builder::addJob);
+        return builder.build();
+    }
+
+    private static void resolveJobDependencies(Map<String, Step> stepMap, Map<String, Job> jobMap) {
+        stepMap.forEach((name, step) -> {
+            step.getWaitfor()
+                .forEach(waitJobName -> {
+                    Job waitJob = jobMap.get(waitJobName);
+                    Job job = jobMap.get(name);
+                    job.waitFor(waitJob);
+                });
+        });
+    }
+
+    private static void fillJobMapWithoutDependencies(Map<String, Step> stepMap, Map<String, Job> jobMap) {
+        stepMap.keySet()
+               .forEach(name -> {
+                   Step step = stepMap.get(name);
+                   StepExecutorConverter converter = new StepExecutorConverter(step);
+                   ShellExecutor executor = converter.asShellExecutor();
+
+                   jobMap.put(name, new Job(name, executor, new FinishNotifier() {
+                   }));
+               });
+    }
+}

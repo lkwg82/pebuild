6a8b785e6b75c9d99bbe978cbdf2baf83469604b
Lars K.W. Gohlke <lkwg82@gmx.de>
2018-12-28 14:31:27 +0100

    cleanup

--- src/test/java/de/lgohlke/ci/config/BuildConfigReaderTest.java
+++ src/test/java/de/lgohlke/ci/config/BuildConfigReaderTest.java
@@ -4,20 +4,21 @@ import de.lgohlke.ci.config.dto.BuildConfig;
 import org.junit.jupiter.api.Test;
 
 class BuildConfigReaderTest {
-    private String yaml = "" +
-            "options: \n" +
-            "  timeout: 10m\n" +
-            "\n" +
-            "steps:\n" +
-            "- name: demo\n" +
-            "  command: 'date'\n" +
-            "- name: sleep\n" +
-            "  command: 'sleep 2'\n" +
-            "  timeout: 10s\n" +
-            "  waitfor: ['demo']";
 
     @Test
     void shouldParse() {
+        String yaml = "" +
+                "options: \n" +
+                "  timeout: 10m\n" +
+                "\n" +
+                "steps:\n" +
+                "- name: demo\n" +
+                "  command: 'date'\n" +
+                "- name: sleep\n" +
+                "  command: 'sleep 2'\n" +
+                "  timeout: 10s\n" +
+                "  waitfor: ['demo']";
+
         BuildConfig buildConfig = BuildConfigReader.parse(yaml);
 
         System.out.println(buildConfig);

80dc69fdb70d6f93234d6e1cde8f3c343d5cd551
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-01-01 16:48:10 +0100

    aligns Channel more with golang channels

--- src/main/java/de/lgohlke/pebuild/Channel.java
+++ src/main/java/de/lgohlke/pebuild/Channel.java
@@ -1,15 +1,24 @@
 package de.lgohlke.pebuild;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.LinkedTransferQueue;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TransferQueue;
+import java.util.concurrent.atomic.AtomicInteger;
 
-class Channel<T> implements AutoCloseable {
+@Slf4j
+class Channel<T> implements AutoCloseable, java.nio.channels.Channel {
 
     private final TransferQueue<T> channel = new LinkedTransferQueue<>();
+    private final AtomicInteger consumers = new AtomicInteger();
 
     private final CountDownLatch numberOfProducers;
 
+    /**
+     * closes automatically when number of producers reached ZERO
+     */
     Channel(int numberOfProducers) {
         this.numberOfProducers = new CountDownLatch(numberOfProducers);
     }
@@ -18,15 +27,30 @@ class Channel<T> implements AutoCloseable {
         if (!isOpen()) {
             throw new ChannelClosedException();
         }
+        if (consumers.get() == 0) {
+            throw new NoConsumerException();
+        }
         channel.offer(element);
     }
 
+    @Override
     public boolean isOpen() {
         return numberOfProducers.getCount() > 0;
     }
 
     public T receive() throws InterruptedException {
-        return channel.take();
+        try {
+            consumers.incrementAndGet();
+            while (isOpen()) {
+                T item = channel.poll(10, TimeUnit.MILLISECONDS);
+                if (null != item) {
+                    return item;
+                }
+            }
+            throw new InterruptedException("channel closed");
+        } finally {
+            consumers.decrementAndGet();
+        }
     }
 
     @Override
@@ -35,5 +59,8 @@ class Channel<T> implements AutoCloseable {
     }
 
     public static class ChannelClosedException extends RuntimeException {
+    }
+
+    public static class NoConsumerException extends RuntimeException {
     }
 }
--- src/test/kotlin/de/lgohlke/pebuild/ChannelTest.kt
+++ src/test/kotlin/de/lgohlke/pebuild/ChannelTest.kt
@@ -3,9 +3,19 @@ package de.lgohlke.pebuild;
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.fail
+import java.util.concurrent.Executors
+import java.util.concurrent.LinkedTransferQueue
+import java.util.concurrent.TimeUnit
 
 class ChannelTest {
     private val channel = Channel<String>(2)
+    private val service = Executors.newFixedThreadPool(1)
+    private val received = LinkedTransferQueue<String>()
+    private val receiver = Runnable {
+        while (true) {
+            received.put(channel.receive())
+        }
+    }
 
     @Test
     fun `is open`() {
@@ -22,25 +32,35 @@ class ChannelTest {
 
     @Test
     fun `happy path`() {
+        startReceiver()
+
         channel.send("hello")
         channel.send("world")
 
-        assertThat(channel.receive()).isEqualTo("hello")
-        assertThat(channel.receive()).isEqualTo("world")
+        assertThat(received.take()).isEqualTo("hello")
+        assertThat(received.take()).isEqualTo("world")
+    }
+
+    private fun startReceiver() {
+        service.submit(receiver)
+        TimeUnit.MILLISECONDS.sleep(10)
     }
 
     @Test
     fun `partial closed`() {
+        startReceiver()
         channel.send("hello")
         channel.close()
         channel.send("world")
 
-        assertThat(channel.receive()).isEqualTo("hello")
-        assertThat(channel.receive()).isEqualTo("world")
+        assertThat(received.take()).isEqualTo("hello")
+        assertThat(received.take()).isEqualTo("world")
     }
 
     @Test
     fun `should not be able to send after closed`() {
+        startReceiver()
+        
         channel.send("hello")
         channel.close()
         channel.close()
@@ -53,4 +73,13 @@ class ChannelTest {
         }
     }
 
+    @Test
+    fun `should not be able to send when there is no consumer`() {
+        try {
+            channel.send("hello")
+            fail("should fail when consumer is missing")
+        } catch (e: Channel.NoConsumerException) {
+            // ok
+        }
+    }
 }

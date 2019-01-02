0fef0e6b9d83d42d657d613cc994eb6a218a2f7c
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-01-02 10:49:30 +0100

    fixes #1 (#2)
    * fixes #1
    
    * Update run.sh
    
    * fixes ShellExecutorTest to be able to run on OSX
    
    * adds option to fail fast the build
    
    * fix PATH to native-image
    
    * fix osx native-image build

--- run.sh
+++ run.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 
-set -e
+set -ex
 
-mvn clean verify
+#mvn clean verify
 
+unamestr=`uname`
 if [[ ! -f .graalvm/graalvm-ce.tar.gz ]]; then
     mkdir -p .graalvm
-    wget -O .graalvm/graalvm-ce.tar.gz https://github.com/oracle/graal/releases/download/vm-1.0.0-rc10/graalvm-ce-1.0.0-rc10-linux-amd64.tar.gz
+    if [[ "$unamestr" == 'Linux' ]]; then
+      url=https://github.com/oracle/graal/releases/download/vm-1.0.0-rc10/graalvm-ce-1.0.0-rc10-linux-amd64.tar.gz
+    elif [[ "$unamestr" == 'Darwin' ]]; then
+      url=https://github.com/oracle/graal/releases/download/vm-1.0.0-rc10/graalvm-ce-1.0.0-rc10-macos-amd64.tar.gz
+    fi
+
+    wget -O .graalvm/graalvm-ce.tar.gz ${url}
     tar -xzf .graalvm/graalvm-ce.tar.gz -C ./.graalvm
 fi
-localGraalVMBin=$(find .graalvm/ -maxdepth 1 -type d | tail -1)
+
+if [[ "$unamestr" == 'Linux' ]]; then
+  localGraalVMBin=$(find .graalvm/ -maxdepth 1 -type d | tail -1)
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  localGraalVMBin=$(find .graalvm/ -maxdepth 1 -type d | tail -1)/Contents/Home
+fi
 export PATH=$PWD/${localGraalVMBin}/bin:$PATH
 
 # wait to not interfere with ide
@@ -17,23 +29,25 @@ sleep 3
 
 classPathOfJar=$(find target/classes/lib -type f| sort | xargs | tr ' ' ':')
 
-set -x
+# osx can not build static
 native-image \
+    $([[ "$unamestr" == 'Linux' ]] && echo -n "--static" || echo -n "") \
     --no-server \
-    --static \
     --class-path target/classes:${classPathOfJar} \
     -H:+ReportUnsupportedElementsAtRuntime \
     -H:ReflectionConfigurationFiles=graalvm.reflections.json \
     -H:Path=target \
     -H:Name="pebuild" \
     de.lgohlke.pebuild.Main
-set +x
-
 
 # only for release
 # upx -v target/pbuild
 
-/usr/bin/time -v ./target/pebuild date
+if [[ "$unamestr" == 'Linux' ]]; then
+  /usr/bin/time -v ./target/pebuild date
+elif [[ "$unamestr" == 'Darwin' ]]; then
+  /usr/bin/time -lp ./target/pebuild date
+fi
 
 if [[ $? == 0 ]]; then
     echo "build ok"

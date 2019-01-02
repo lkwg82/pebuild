f82dacc13ab0004fe1db3610bbc2d34fa6e809f2
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-01-02 22:57:06 +0100

    separated unit and it from ShellExecutorTest

--- src/test/java/de/lgohlke/pebuild/ShellExecutorTest.java
+++ src/test/java/de/lgohlke/pebuild/ShellExecutorTest.java
@@ -1,14 +1,8 @@
 package de.lgohlke.pebuild;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.time.Duration;
-import java.util.Random;
 
 import static java.time.temporal.ChronoUnit.MILLIS;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -50,32 +44,5 @@ class ShellExecutorTest {
         ExecutionResult result = ShellExecutor.execute3(command, Duration.of(100, MILLIS));
 
         assertThat(result.getExitCode()).isEqualTo(3);
-    }
-
-    static {
-        System.setProperty("org.slf4j.simpleLogger.log.de.lgohlke.pebuild.Channel", "DEBUG");
-        System.setProperty("org.slf4j.simpleLogger.log.de.lgohlke.pebuild.CombinedStreamFascade", "DEBUG");
-    }
-
-    @Test
-    @RepeatedTest(100)
-    void captureOutputAsFile() throws Exception {
-        Path path = Files.createTempDirectory(new Random().nextInt() + "");
-        Configuration.REPORT_DIRECTORY.overwrite(path.toAbsolutePath()
-                                                     .toString());
-        try {
-            ShellExecutor shellExecutor = new ShellExecutor("test",
-                                                            "echo hello err >&2; echo hello out",
-                                                            Duration.ZERO,
-                                                            new JobTrigger("test"));
-
-            shellExecutor.runCommand();
-
-            Path output = Paths.get(Configuration.REPORT_DIRECTORY.value(), "step.test.output");
-            String content = new String(Files.readAllBytes(output));
-            assertThat(content).contains("hello out");
-        } finally {
-            FileUtils.deleteDirectory(path.toFile());
-        }
     }
 }

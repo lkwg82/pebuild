815b40a45747a1841a4374c3f16b58a56cdfc03e
Lars Gohlke <lars.gohlke@idealo.de>
2019-01-10 20:10:44 +0100

    adds TiniDownloader

---
+++ app/src/main/kotlin/de/lgohlke/pebuild/TiniDownloader.kt
@@ -0,0 +1,26 @@
+package de.lgohlke.pebuild
+
+import org.apache.commons.io.FileUtils
+import java.io.File
+import java.net.URL
+import java.nio.file.Path
+
+class TiniDownloader(private val path: String = System.getenv("PATH")) {
+    fun hasTini(): Boolean {
+        val builder = ProcessBuilder("which", "tiniPath")
+        val env = builder.environment()
+        env["PATH"] = path
+        val process = builder.start()
+
+        return process.waitFor() == 0
+    }
+
+    fun download(tini: Path) {
+        // ~23kb
+        val url = URL("https://github.com/krallin/tiniPath/releases/download/v0.18.0/tiniPath-amd64")
+        FileUtils.copyURLToFile(url, tini.toFile(), 2000, 2000)
+
+        File(tini.toFile().absolutePath).setExecutable(true)
+    }
+
+}
---
+++ app/src/test/java/de/lgohlke/pebuild/TiniDowloaderTest.kt
@@ -0,0 +1,47 @@
+package de.lgohlke.pebuild
+
+import org.apache.commons.io.FileUtils
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.AfterEach
+import org.junit.jupiter.api.Test
+import java.io.File
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.security.SecureRandom
+
+class TiniDowloaderTest {
+    private val tempDirectory = Files.createTempDirectory(SecureRandom().nextDouble().toString())
+    private val tmpDir = tempDirectory.toFile().absolutePath
+    private val tiniPath = Paths.get(tmpDir, "tiniPath")
+
+    @AfterEach
+    internal fun tearDown() {
+        FileUtils.deleteDirectory(tempDirectory.toFile())
+    }
+
+    @Test
+    fun `should not find tini`() {
+        val downloader = TiniDownloader("/bin")
+
+        assertThat(downloader.hasTini()).isFalse()
+    }
+
+    @Test
+    fun `should find tini`() {
+        Files.write(tiniPath, "".toByteArray())
+        File(tiniPath.toFile().absolutePath).setExecutable(true)
+
+        val downloader = TiniDownloader("/bin:$tmpDir")
+
+        assertThat(downloader.hasTini()).isTrue()
+    }
+
+    @Test
+    fun `should download tini`() {
+        TiniDownloader().download(tiniPath)
+
+        assertThat(tiniPath).exists()
+        assertThat(tiniPath).isExecutable()
+    }
+
+}

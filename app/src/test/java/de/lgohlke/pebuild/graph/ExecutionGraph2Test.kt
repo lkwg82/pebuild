6e9bae4bd278dc9f1138d75ad811e28277a856d5
Lars K.W. Gohlke <lkwg82@gmx.de>
2019-01-26 22:35:53 +0100

    cleanup

--- app/src/test/java/de/lgohlke/pebuild/graph/ExecutionGraph2Test.kt
+++ app/src/test/java/de/lgohlke/pebuild/graph/ExecutionGraph2Test.kt
@@ -4,16 +4,12 @@ import de.lgohlke.pebuild.ExecutionResult
 import de.lgohlke.pebuild.JobTrigger
 import de.lgohlke.pebuild.StepExecutor
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.RepeatedTest
 import org.junit.jupiter.api.Test
-import org.slf4j.LoggerFactory
-import reactor.core.CoreSubscriber
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.publisher.toFlux
 import reactor.core.scheduler.Schedulers.parallel
-import reactor.test.test
 import java.time.Duration
 import java.time.Duration.ZERO
 import java.util.*
@@ -25,54 +21,9 @@ import kotlin.collections.LinkedHashMap
 import kotlin.collections.set
 
 class ExecutionGraph2Test {
-    private val root = Node("root", setOf(), true)
-    private val nodes = ArrayList<Node>()
-
-    private val a = Node("A", root)
-    private val c = Node("C", a)
-    private val e = Node("E", a, c)
-    private val d = Node("D", e)
-    private val f = Node("F", e)
-    private val b = Node("B", e, f)
-    private var end = Node("end")
-
-    @BeforeEach
-    internal fun setUp() {
-        nodes.addAll(setOf(root, a, b, c, d, e, f))
-
-        val endNodes = createSuccessorMap()
-                .filter { (_, successors) -> successors.isEmpty() }
-                .map { (node, _) -> node }
-        end = Node("end", endNodes.toSet())
-
-        nodes.add(end)
-    }
-
-    @Test
-    fun cachedMono() {
-        val counter = LongAdder()
-        val mono = object : Mono<String>() {
-            override fun subscribe(p0: CoreSubscriber<in String>) {
-                counter.increment()
-                p0.onNext("executed " + counter.toInt())
-                p0.onComplete()
-            }
-        }
-
-        val cache = mono.cache()
-
-        cache.test()
-                .expectNext("executed 1")
-                .expectComplete()
-                .verifyThenAssertThat(Duration.ofMillis(300))
-        cache.test()
-                .expectNext("executed 1")
-                .expectComplete()
-                .verifyThenAssertThat(Duration.ofMillis(300))
-    }
 
     @RepeatedTest(100)
-    fun `should execute in correct order`() {
+    fun `should execute in correct orders`() {
         val executions = Collections.synchronizedList(ArrayList<String>())
 
         val a = DummyExecutor("a", executions)
@@ -99,25 +50,6 @@ class ExecutionGraph2Test {
         } catch (e: AssertionError) {
             assertThat(executions).endsWith("f", "b", "d")
         }
-    }
-
-    @Test
-    fun `should run with stepExecutors`() {
-
-        val executions = Collections.synchronizedList(ArrayList<String>())
-
-        val s1 = DummyExecutor("s1", executions)
-        val s2 = DummyExecutor("s2", executions)
-
-        s2.waitFor(s1)
-
-        ExecutionGraph2.Builder()
-                .addJob(s1)
-                .addJob(s2)
-                .build()
-                .execute()
-
-        assertThat(executions).containsExactly("s1", "s2")
     }
 
     @Test
@@ -171,55 +103,6 @@ class ExecutionGraph2Test {
         override fun runCommand(): ExecutionResult {
             executions.add(name2)
             return ExecutionResult(0)
-        }
-    }
-
-    private fun createExecutionGraph(node: Node,
-                                     cachedNodes: Map<Node, Mono<Node>>): Flux<Node> {
-        val thisNode = cachedNodes[node]?.toFlux() ?: throw IllegalStateException("missing node in cached map: $node")
-        val preFluxes = node.predecessors.map { predecessor -> createExecutionGraph(predecessor, cachedNodes) }
-        val merge = Flux.merge(Flux.fromIterable(preFluxes))
-        return Flux.concat(merge, thisNode)
-    }
-
-    private fun createSuccessorMap(): Map<Node, Set<Node>> {
-        val successorMap = LinkedHashMap<Node, MutableSet<Node>>()
-
-        nodes.forEach { node ->
-            successorMap[node] = LinkedHashSet()
-        }
-
-        nodes.forEach { node ->
-            node.predecessors.forEach { n ->
-                successorMap[n]?.add(node)
-            }
-        }
-        return successorMap
-    }
-
-    class Node(private val name: String,
-               val predecessors: Set<Node> = setOf(),
-               private var terminated: Boolean = false) {
-
-        constructor(name: String,
-                    vararg predecessors: Node) : this(name, predecessors.toSet())
-
-        companion object {
-            private val log = LoggerFactory.getLogger(this::class.java.enclosingClass)
-        }
-
-        fun execute() {
-            val t = Thread.currentThread()
-                    .name
-            log.warn("execute {} on {}", name, t)
-        }
-
-        override fun toString(): String {
-            var str = name
-            if (terminated) {
-                str += "*"
-            }
-            return str
         }
     }
 }

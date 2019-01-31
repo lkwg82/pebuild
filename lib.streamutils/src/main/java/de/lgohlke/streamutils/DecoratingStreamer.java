905eb89c0aad2ab5de35c27a53600c807664b7ad
Lars Gohlke <lars.gohlke@idealo.de>
2019-01-31 02:22:28 +0100

    cleanup

--- lib.streamutils/src/main/java/de/lgohlke/streamutils/DecoratingStreamer.java
+++ lib.streamutils/src/main/java/de/lgohlke/streamutils/DecoratingStreamer.java
@@ -3,7 +3,6 @@ package de.lgohlke.streamutils;
 import lombok.extern.slf4j.Slf4j;
 import lombok.val;
 
-import java.io.InputStream;
 import java.util.Scanner;
 
 @Slf4j
@@ -11,14 +10,6 @@ class DecoratingStreamer {
     private final Channel<String> channel;
     private final NotifyWaiter notifyWaiter;
     private final PrefixedInputStream prefixedInputStream;
-
-    @Deprecated
-    DecoratingStreamer(InputStream inputStream,
-                       String prefix,
-                       Channel<String> channel,
-                       NotifyWaiter notifyWaiter) {
-        this(new PrefixedInputStream(inputStream, prefix), channel, notifyWaiter);
-    }
 
     DecoratingStreamer(PrefixedInputStream prefixedInputStream,
                        Channel<String> channel,
@@ -31,27 +22,21 @@ class DecoratingStreamer {
 
     void capture() {
         notifyWaiter.waitForReceiverStarted();
+        String prefix = prefixedInputStream.getPrefix();
 
         try (val scanner = new Scanner(prefixedInputStream.getStream())) {
-            String prefix = prefixedInputStream.getPrefix();
 
             log.debug("scanner started: {}", prefix);
             notifyWaiter.notifySenderStarted();
 
-            sendLinesOverChannel(scanner);
-
-            log.debug("scanner closed: {}", prefix);
-            notifyWaiter.notifySenderStopped();
+            while (scanner.hasNextLine()) {
+                channel.send(prefix + " " + scanner.nextLine());
+            }
 
         } catch (Exception e) {
             log.error(e.getMessage(), e);
         }
-    }
-
-    private void sendLinesOverChannel(Scanner sc) {
-        String prefix = prefixedInputStream.getPrefix();
-        while (sc.hasNextLine()) {
-            channel.send(prefix + " " + sc.nextLine());
-        }
+        log.debug("scanner closed: {}", prefix);
+        notifyWaiter.notifySenderStopped();
     }
 }
